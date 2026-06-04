008a9e0189944012b0f17be8f27903bab70fdd17
João Pedro Navarro Okita <joao.okita07@unifesp.br>
2026-06-04 02:17:56 -0300

    Adição da variável DEFAULT_TICKETS

--- minix/kernel/proc.h
+++ minix/kernel/proc.h
@@ -140,6 +140,9 @@ struct proc {
 
 #endif /* __ASSEMBLY__ */
 
+/* Número padrão de tickets para processos */
+#define DEFAULT_TICKETS 100
+
 /* Bits for the runtime flags. A process is runnable iff p_rts_flags == 0. */
 #define RTS_SLOT_FREE	0x01	/* process slot is free */
 #define RTS_PROC_STOP	0x02	/* process has been stopped */
